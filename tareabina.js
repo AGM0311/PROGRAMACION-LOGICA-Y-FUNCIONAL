54c1f06df89a81a84887822ec67c6b5f72def3e3
AXL GAMAS MORENO <axl0311@MacBook-Air-de-AXL.local>
2026-07-07 09:10:08 -0600

    programación lógica

--- tareabina.js
+++ tareabina.js
@@ -1,4 +1,4 @@
-var historialCommits = [
+const historialCommits = [
   { version: 1.0, ambiente: "desarrollo" },
   { version: 1.1, ambiente: "desarrollo" },
   { version: 1.2, ambiente: "testing" },
@@ -10,18 +10,19 @@ var historialCommits = [
 
 const esProduccion=(commit)=>commit.ambiente==="produccion";
 
+const axlit=({ version })=>version;
+
 function* generadora(iterable, predicado) {
   for (let dato of iterable) {
     if (predicado(dato)) {
-      yield dato;
+      yield axlit(dato);
     }
   }
 }
-const generador=generadora(historialCommits,esProduccion);
-const primero = generador.next().value;
 
-console.log("Primer commit en produccion: version " + primero.version);
+const gen = generadora(historialCommits, esProduccion);
+console.log("Primer commit en produccion: version " + gen.next().value);
 
 const n = 1000000;
-const maximo = Math.ceil(Math.log(n) / Math.log(2));
-console.log("Para " + n + " commits, maximo " + maximo + " evaluaciones");
+const maximo = Math.ceil(Math.log2(n));
+console.log("Para "+n+"commits,maximo"+maximo+"evaluaciones");
